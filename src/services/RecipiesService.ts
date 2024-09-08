ae934bb256a68b60e0a634599b30fabb0a3d7c5a
EliDev <oscarmateoelias@gmail.com>
2024-09-08 15:59:45 -0600

    Add recipe categories to state and fetch them from API

---
+++ src/services/RecipiesService.ts
@@ -0,0 +1,3 @@
+export async function getCategories()  {
+    console.log('www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+}
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,4 +1,7 @@
+import { useAppStore } from "../store/useAppStore";
+
 export default function IndexPage() {
+  useAppStore((state) => state.categories);
   return (
     <>
       <h1> Hello Index</h1>
